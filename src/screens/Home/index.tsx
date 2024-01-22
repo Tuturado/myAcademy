895b32bd55821450ffc655dc5e308d64e8483faa
Tuturado <alef23073@gmail.com>
2024-01-22 15:50:35 -0300

    Update: Posicionado o Modal de Deleção de exercício

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, FlatList, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Button, FlatList, Modal, Pressable, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
@@ -9,11 +9,16 @@ import { TrainingSheet } from '../TrainingSheet';
 function Home() {
   const navigation: NavigationProp<ReactNavigation.RootParamList> = useNavigation();
   const [treinos, setTreinos] = useState<any>([]);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
 
   const handleSignOut = () => {
     auth().signOut();
   };
 
+  const excludeTraining = (id:any) =>{
+    console.log('ID: ', id);
+    setModalVisible(true);
+  };
 
   useEffect(() => {
     const subscriber = firestore()
@@ -31,12 +36,33 @@ function Home() {
     return () => subscriber();
   }, []);
 
-  
+  useEffect(() => {
+    console.log('MODAL VISIBLE: ', modalVisible);
+  }, [modalVisible])
 
-  function renderList(item:any){
+
+  function renderList(item: any) {
 
     const Item = ({ diaSemana, nome, periodo, peso, repeticoes, series, status }: TrainingSheet) => (
       <View style={styles.item} >
+        <View style={styles.topButtonsContainer}>
+
+
+
+          <View style={styles.exitButtonContainer}>
+            <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.updateTrainingButton}>
+              <Text style={styles.text}>Atualizar Treino</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.exitButtonContainer}>
+            <TouchableOpacity onPress={() => excludeTraining(item?.id)} style={styles.excludeTrainingButton}>
+              <Text style={styles.text}>Excluir Treino</Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
+
         <Text>Dia da Semana: {diaSemana}</Text>
         <Text>Nome do Exercício: {nome}</Text>
         <Text>Período de treino: {periodo}</Text>
@@ -47,36 +73,64 @@ function Home() {
       </View>
     );
 
-    return(
+    return (
       <Item diaSemana={item?.fichaTreino?.diaSemana} nome={item?.fichaTreino?.nome} periodo={item?.fichaTreino?.periodo} peso={item?.fichaTreino?.peso} repeticoes={item?.fichaTreino?.repeticoes} series={item?.fichaTreino?.series} status={item?.fichaTreino?.status} />
-    ) 
+    )
   }
 
 
   return (
     <>
-    
-        <View style={styles.container}>
-          <View style={styles.exitButtonContainer}>
-            <TouchableOpacity onPress={() => handleSignOut()} style={styles.exitButton}>
-              <Text style={styles.text}>Sair</Text>
-            </TouchableOpacity>
-          </View>
-          
-          {treinos.length != 0 ? <FlatList
-            data={treinos}
-            renderItem={({ item }) => renderList(item)}
-            keyExtractor={item => item?.id}
-          /> : <Text>Ainda não existem treinos cadastrados, clique no botão para adicionar!</Text>}
 
-          <View style={styles.plusButtonContainer}>
-            <TouchableOpacity onPress={() => navigation.navigate('newTrainingSheet')} style={styles.plusButtom}>
-              <Text style={styles.textButton}>+</Text>
-            </TouchableOpacity>
-          </View>
-
+      <View style={styles.container}>
+        <View style={styles.exitButtonContainer}>
+          <TouchableOpacity onPress={() => handleSignOut()} style={styles.exitButton}>
+            <Text style={styles.text}>Sair</Text>
+          </TouchableOpacity>
         </View>
-     
+
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.text}>Tem Certeza que deseja excluir esse treino?</Text>
+              <View style={styles.pressedContainer}>
+                <Pressable
+                  style={[styles.button, styles.buttonExclude]}
+                  onPress={() => setModalVisible(!modalVisible)}>
+                  <Text style={{ color: 'black' }}>SIM!</Text>
+                </Pressable>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}>
+                  <Text style={{ color: 'white' }}>NÃO!</Text>
+                </Pressable>
+              </View>
+
+
+            </View>
+          </View>
+        </Modal>
+
+        {treinos.length != 0 ? <FlatList
+          data={treinos}
+          renderItem={({ item }) => renderList(item)}
+          keyExtractor={item => item?.id}
+        /> : <Text>Ainda não existem treinos cadastrados, clique no botão para adicionar!</Text>}
+
+        <View style={styles.plusButtonContainer}>
+          <TouchableOpacity onPress={() => navigation.navigate('newTrainingSheet')} style={styles.plusButtom}>
+            <Text style={styles.textButton}>+</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+
     </>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-        flex:1,
+        flex: 1,
         display: 'flex',
         flexWrap: 'nowrap',
         margin: 0,
@@ -19,7 +19,7 @@ export const styles = StyleSheet.create({
         color: 'white',
     },
     exitButtonContainer: {
-        padding: 20,
+        padding: 10,
         alignItems: 'flex-end'
     },
     exitButton: {
@@ -29,6 +29,65 @@ export const styles = StyleSheet.create({
         justifyContent: 'center',
         width: '30%',
         height: 30,
+    },
+    topButtonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    excludeTrainingButton: {
+        backgroundColor: 'red',
+        alignItems: 'center',
+        alignContent: 'center',
+        justifyContent: 'center',
+        height: 30,
+    },
+    updateTrainingButton: {
+        backgroundColor: 'green',
+        alignItems: 'center',
+        alignContent: 'center',
+        justifyContent: 'center',
+        height: 30,
+    },
+    modalView: {
+        width: '70%',
+        height: '50%',
+        color: 'white',
+        margin: 20,
+        backgroundColor: 'red',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    centeredView: {
+        color: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+        marginTop: '70%',
+    },
+    pressedContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    button: {
+        borderRadius: 10,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonClose: {
+        backgroundColor: 'blue',
+    },
+    buttonExclude: {
+        backgroundColor: 'yellow',
     },
     plusButtonContainer: {
         paddingRight: 20,
